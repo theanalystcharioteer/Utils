109a25d893126631d7a0c87d81624b77ac2778ae
Pinaki Brahma <42740963+theanalystcharioteer@users.noreply.github.com>
2023-07-08 12:31:00 +0530

    added style

--- color_text.py
+++ color_text.py
@@ -1,29 +1,22 @@
 class ColorText:
-    def text_in_color(txt:str, color_font_name:str=None, color_bg_name:str=None):
+    def text_in_color(txt:str, color_font_name:str=None, color_bg_name:str=None, style_name:str='normal'):
         '''
         output the text with a foreground and a background color
         args:
             `txt`(str): text to be printed
             `color_font_name`(str): font color amongst ['black', 'red', 'green', 'yellow', 'blue', 'pink', 'cyan', 'grey']
             `color_bg_name`(str): back ground color amongst ['black', 'red', 'green', 'yellow', 'blue', 'pink', 'cyan', 'grey']
+            `style_name`(str): ['normal', 'bold', 'light', 'italics', 'underline', 'blink']
         '''
         colors = ['black', 'red', 'green', 'yellow', 'blue', 'pink', 'cyan', 'grey']
         dict_color_map_font = {k:v for k,v in zip(colors, range(30,38))}
         dict_color_map_bg = {k:v for k,v in zip(colors, range(40,48))}
+        dict_styles = {'normal':0, 'bold':1, 'light':2, 'italics':3, 'underline':4, 'blink':5}
         color_font = dict_color_map_font.get(color_font_name, 30)
         color_bg = dict_color_map_bg.get(color_bg_name, 49)
+        style_font = dict_styles.get(style_name, 0)
 
-        txt_w_col = f"{txt}"
-
-        if not color_font:
-            txt_w_col = f"{txt_w_col}"
-        else:
-            txt_w_col = f"\x1b[{color_font}m{txt_w_col}"
-
-        if not color_bg:
-            txt_w_col = f"{txt_w_col}"
-        else:
-            txt_w_col = f"\x1b[{color_bg}m{txt_w_col}"
+        txt_w_col = f"\x1b[{style_font};{color_font};{color_bg}m{txt}"
 
         return txt_w_col
 
